6e6e248fe3a52324cbccbda77b523bbffc8824a2
Javaid Iqbal <abubakarsattar8@gmail.com>
2021-08-13 16:24:19 +0500

    -Home page Layout

---
+++ app/src/main/java/com/appsuite/longscreenshot/MainActivity.java
@@ -0,0 +1,64 @@
+package com.appsuite.longscreenshot;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Menu;
+import android.widget.ImageView;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.appsuite.longscreenshot.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        getSupportActionBar().hide();
+//        DrawerLayout drawer = binding.drawerLayout;
+
+
+
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ImageView menuIcon = (ImageView) findViewById(R.id.logo);
+        menuIcon.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                drawer.openDrawer(Gravity.LEFT);
+            }
+        });
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+}
